410235f031e6eaecebf3b0691defe1acc09e984d
Guilherme <jogos.coisas@gmail.com>
2020-11-18 22:30:18 -0300

    format code dart

--- lib/g2x_custom_toaster.dart
+++ lib/g2x_custom_toaster.dart
@@ -5,75 +5,76 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-class G2xCustomToaster{
+class G2xCustomToaster {
   /// Show toaste on top
   ///
   /// [navigationKey] navigation key inserted in the mainapp(MaterialApp)
-  /// 
+  ///
   /// [milliseconds to discard] is the animation duration.
-  /// 
+  ///
   /// [onFinish] is called when finish animation.
-  static void showOnTop({
-      @required IconData icon,
+  static void showOnTop(
+      {@required IconData icon,
       @required String title,
       @required String mensage,
       @required GlobalKey<NavigatorState> navigationKey,
       int millisecondsToStart = 200,
       int millisecondsToDismissAnd = 1000,
       Function onFinish,
-      Function onTap
-    }){
-      _navigationKey = navigationKey;
-      _overlayEntryForRemove.add(
-        _buildEntry(icon,title, mensage,
+      Function onTap}) {
+    _navigationKey = navigationKey;
+    _overlayEntryForRemove.add(_buildEntry(
+        icon,
+        title,
+        mensage,
         millisecondsToStart,
         millisecondsToDismissAnd,
-        onFinish, onTap, _overlayEntryForRemove.length));
-      if(_overlayEntryForRemove.length == 1){
-        _insertNext(navigationKey);
-      }
+        onFinish,
+        onTap,
+        _overlayEntryForRemove.length));
+    if (_overlayEntryForRemove.length == 1) {
+      _insertNext(navigationKey);
+    }
   }
 
   static GlobalKey<NavigatorState> _navigationKey;
 
-  static _insertNext(GlobalKey<NavigatorState> navigationKey){
-    if(_overlayEntryForRemove.length > 0)
+  static _insertNext(GlobalKey<NavigatorState> navigationKey) {
+    if (_overlayEntryForRemove.length > 0)
       navigationKey.currentState.overlay.insert(_overlayEntryForRemove[0]);
   }
 
-  static _removeFirst(){
+  static _removeFirst() {
     _overlayEntryForRemove[0].remove();
     _overlayEntryForRemove.removeAt(0);
     _insertNext(_navigationKey);
   }
-  
+
   static var _overlayEntryForRemove = List<OverlayEntry>();
   static OverlayEntry _buildEntry(
-    IconData icon,
-    String title,
-    String mensage,
-    int millisecondsToStart,
-    int millisecondsToDismissAnd,
-    Function onFinish,
-    Function onTap,
-    int index
-  ){
-    var _overlayEntry = OverlayEntry(builder: (BuildContext buildContext){
+      IconData icon,
+      String title,
+      String mensage,
+      int millisecondsToStart,
+      int millisecondsToDismissAnd,
+      Function onFinish,
+      Function onTap,
+      int index) {
+    var _overlayEntry = OverlayEntry(builder: (BuildContext buildContext) {
       return _G2xCustomToasterComponent(
-        key: UniqueKey(),
-        title: title,
-        mensage: mensage,
-        millisecondsToStart: millisecondsToStart,
-        millisecondsToDismissAnd: millisecondsToDismissAnd,
-        onFinish: (){
-          if(onFinish != null){
-            onFinish();
-          }
-         _removeFirst();
-        },
-        onTap: onTap,
-        icon: icon
-      );
+          key: UniqueKey(),
+          title: title,
+          mensage: mensage,
+          millisecondsToStart: millisecondsToStart,
+          millisecondsToDismissAnd: millisecondsToDismissAnd,
+          onFinish: () {
+            if (onFinish != null) {
+              onFinish();
+            }
+            _removeFirst();
+          },
+          onTap: onTap,
+          icon: icon);
     });
     return _overlayEntry;
   }
@@ -87,17 +88,23 @@ class _G2xCustomToasterComponent extends StatefulWidget {
   final Function onFinish;
   final Function onTap;
   final IconData icon;
-  const _G2xCustomToasterComponent({
-    Key key, this.title, this.mensage,
-    this.millisecondsToStart,
-    this.millisecondsToDismissAnd,
-    this.onFinish, this.onTap,this.icon
-  }) : super(key: key);
+  const _G2xCustomToasterComponent(
+      {Key key,
+      this.title,
+      this.mensage,
+      this.millisecondsToStart,
+      this.millisecondsToDismissAnd,
+      this.onFinish,
+      this.onTap,
+      this.icon})
+      : super(key: key);
   @override
-  __G2xCustomToasterComponentState createState() => __G2xCustomToasterComponentState();
+  __G2xCustomToasterComponentState createState() =>
+      __G2xCustomToasterComponentState();
 }
 
-class __G2xCustomToasterComponentState extends State<_G2xCustomToasterComponent> with TickerProviderStateMixin {
+class __G2xCustomToasterComponentState extends State<_G2xCustomToasterComponent>
+    with TickerProviderStateMixin {
   AnimationController _animationController;
   Animation<double> _animation;
   var _globalKeyCard = GlobalKey();
@@ -110,86 +117,103 @@ class __G2xCustomToasterComponentState extends State<_G2xCustomToasterComponent>
   var sizeScreen = Size.zero;
 
   @override
-  void initState(){
-    Future.delayed(Duration.zero, (){
+  void initState() {
+    Future.delayed(Duration.zero, () {
       _cardSize = _globalKeyCard.currentContext.size;
-      _buildVerticalAniomation(-_cardSize.height, MediaQuery.of(context).padding.top + 5, Curves.linear, false, widget.millisecondsToStart);
+      _buildVerticalAniomation(
+          -_cardSize.height,
+          MediaQuery.of(context).padding.top + 5,
+          Curves.linear,
+          false,
+          widget.millisecondsToStart);
     });
     super.initState();
   }
 
-  _buildVerticalAniomation(double begin, double end, Curve curve, bool finish, int milliseconds, {bool vertical = true}){
-    _animationController = AnimationController(vsync: this, duration: Duration(milliseconds: milliseconds));
-      var _curve = CurvedAnimation(parent: _animationController, curve: curve);
-      _animation = Tween<double>(begin: begin, end: end).animate(_curve);
+  _buildVerticalAniomation(
+      double begin, double end, Curve curve, bool finish, int milliseconds,
+      {bool vertical = true}) {
+    _animationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: milliseconds));
+    var _curve = CurvedAnimation(parent: _animationController, curve: curve);
+    _animation = Tween<double>(begin: begin, end: end).animate(_curve);
 
-      _animationController.addListener(() {
-        if(_animationController.status == AnimationStatus.completed){
-          if(vertical){
-            _currentY = end;
-            if(!finish) _buildTimer();
-          }
-          else{
-            _currentX = end;
-          }
-          if(finish || !vertical){
-            if(widget.onFinish != null) widget.onFinish();
-          }
+    _animationController.addListener(() {
+      if (_animationController.status == AnimationStatus.completed) {
+        if (vertical) {
+          _currentY = end;
+          if (!finish) _buildTimer();
+        } else {
+          _currentX = end;
         }
-        else{
-          if(vertical){
-            _currentY = _animation.value;
-          }
-          else{
-            _currentX = _animation.value;
-          }
+        if (finish || !vertical) {
+          if (widget.onFinish != null) widget.onFinish();
         }
-        setState(() {});
-      });
-      _animationController.forward();
+      } else {
+        if (vertical) {
+          _currentY = _animation.value;
+        } else {
+          _currentX = _animation.value;
+        }
+      }
+      setState(() {});
+    });
+    _animationController.forward();
   }
 
-  _buildTimer(){
+  _buildTimer() {
     _timeToDismiss?.cancel();
-    _timeToDismiss = Timer(Duration(milliseconds: widget.millisecondsToDismissAnd), (){
+    _timeToDismiss =
+        Timer(Duration(milliseconds: widget.millisecondsToDismissAnd), () {
       _dismiss = true;
-      if(!_pressing){
-        _buildVerticalAniomation(MediaQuery.of(context).padding.top + 5, -(_cardSize.height + 20), Curves.elasticIn, true, widget.millisecondsToDismissAnd);
+      if (!_pressing) {
+        _buildVerticalAniomation(
+            MediaQuery.of(context).padding.top + 5,
+            -(_cardSize.height + 20),
+            Curves.elasticIn,
+            true,
+            widget.millisecondsToDismissAnd);
       }
     });
   }
 
-  _buildCard(){
+  _buildCard() {
     return Material(
       color: Colors.transparent,
       child: GestureDetector(
-        onTap: (){
-          if(widget.onTap != null) widget.onTap();
+        onTap: () {
+          if (widget.onTap != null) widget.onTap();
         },
-        onTapDown: (_){
-         _pressing = true;
+        onTapDown: (_) {
+          _pressing = true;
         },
-        onTapUp: (_){
+        onTapUp: (_) {
           _pressing = false;
-          if(_dismiss)
-            _buildVerticalAniomation(MediaQuery.of(context).padding.top + 5, -(_cardSize.height + 20),Curves.elasticIn, true, widget.millisecondsToDismissAnd);
+          if (_dismiss)
+            _buildVerticalAniomation(
+                MediaQuery.of(context).padding.top + 5,
+                -(_cardSize.height + 20),
+                Curves.elasticIn,
+                true,
+                widget.millisecondsToDismissAnd);
         },
-        onHorizontalDragUpdate: (DragUpdateDetails d){
+        onHorizontalDragUpdate: (DragUpdateDetails d) {
           _pressing = true;
           setState(() {
             _currentX += d.primaryDelta;
           });
         },
-        onHorizontalDragEnd: (DragEndDetails d){
+        onHorizontalDragEnd: (DragEndDetails d) {
           _pressing = false;
-          if(_currentX <= -100 || _currentX >= 100){
+          if (_currentX <= -100 || _currentX >= 100) {
             _timeToDismiss.cancel();
             var begin = _currentX;
-            var end = MediaQuery.of(context).size.width * (_currentX < 0 ? -1 : 1);
-            _buildVerticalAniomation(
-              begin, end,Curves.fastLinearToSlowEaseIn, true, widget.millisecondsToDismissAnd, vertical: false); 
-          }
-          else{
+            var end =
+                MediaQuery.of(context).size.width * (_currentX < 0 ? -1 : 1);
+            _buildVerticalAniomation(begin, end, Curves.fastLinearToSlowEaseIn,
+                true, widget.millisecondsToDismissAnd,
+                vertical: false);
+          } else {
             _buildTimer();
             setState(() {
               _currentX = 0;
--- test/g2x_custom_toaster_test.dart
+++ test/g2x_custom_toaster_test.dart
@@ -2,39 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:g2x_custom_toaster/g2x_custom_toaster.dart';
+
 void main() {
   final _navigationKey = GlobalKey<NavigatorState>();
-  _buildInitApp(Widget child){
+  _buildInitApp(Widget child) {
     return MaterialApp(
-      navigatorKey: _navigationKey,
-      home: Scaffold(
-        body: child
-      )
-    );
+        navigatorKey: _navigationKey, home: Scaffold(body: child));
   }
+
   testWidgets("open/close and callbacks toaster", (WidgetTester t) async {
     var countCallbacks = 0;
-    await t.pumpWidget(
-      _buildInitApp(
-        FlatButton(
-          child: Text(""),
-          onPressed: (){
-            G2xCustomToaster.showOnTop(
-              icon: Icons.chat,
-              title: "G2xCustomToaster Title",
-              mensage: "Content",
-              navigationKey: _navigationKey,
-              onTap: (){
-                countCallbacks++;
-              },
-              onFinish: (){
-                countCallbacks++;
-              }
-            );
-          },
-        )
-      )
-    );
+    await t.pumpWidget(_buildInitApp(FlatButton(
+      child: Text(""),
+      onPressed: () {
+        G2xCustomToaster.showOnTop(
+            icon: Icons.chat,
+            title: "G2xCustomToaster Title",
+            mensage: "Content",
+            navigationKey: _navigationKey,
+            onTap: () {
+              countCallbacks++;
+            },
+            onFinish: () {
+              countCallbacks++;
+            });
+      },
+    )));
     await t.tap(find.byType(FlatButton));
     //slide down animation
     await t.pumpAndSettle(const Duration(milliseconds: 200));
@@ -48,26 +41,22 @@ void main() {
     expect(countCallbacks, 2);
   });
 
-  testWidgets("open and manual close and callbacks toaster", (WidgetTester t) async {
+  testWidgets("open and manual close and callbacks toaster",
+      (WidgetTester t) async {
     var countCallbacks = 0;
-    await t.pumpWidget(
-      _buildInitApp(
-        FlatButton(
-          child: Text(""),
-          onPressed: (){
-            G2xCustomToaster.showOnTop(
-              icon: Icons.chat,
-              title: "G2xCustomToaster Title",
-              mensage: "Content",
-              navigationKey: _navigationKey,
-              onFinish: (){
-                countCallbacks++;
-              }
-            );
-          },
-        )
-      )
-    );
+    await t.pumpWidget(_buildInitApp(FlatButton(
+      child: Text(""),
+      onPressed: () {
+        G2xCustomToaster.showOnTop(
+            icon: Icons.chat,
+            title: "G2xCustomToaster Title",
+            mensage: "Content",
+            navigationKey: _navigationKey,
+            onFinish: () {
+              countCallbacks++;
+            });
+      },
+    )));
     await t.tap(find.byType(FlatButton));
     //slide down animation
     await t.pumpAndSettle(const Duration(milliseconds: 200));
